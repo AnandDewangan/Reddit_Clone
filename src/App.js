783d7ef6475db170f57f097875128f9f4d3f74a6
Anand Dewangan <118416821+AnandDewangan@users.noreply.github.com>
2023-03-04 13:43:25 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import Home from './components/Home';
-import 'react-bootstrap';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ToggleContext from './context/ToggleContext';
-import Login from './components/Login';
+import "./App.css";
+import Main from "./components/main/Main";
+import SideNav from "./components/sidenav/SideNav";
 
-const getLocalStorage=()=>{
-  let list = localStorage.getItem("userDetails");
-  return list ? JSON.parse(list) : [];
-}
 
-export default function App() {
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [user, setUser] = useState(getLocalStorage)
-
-  useEffect(()=>{
-    localStorage.setItem("userDetails", JSON.stringify(name));
-  }, [name])
+function App() {
   return (
-    <ToggleContext.Provider value={
-      {
-        name: name,
-        password: password,
-        setName:setName,
-        setPassword: setPassword,
-        user: user,
-        setUser: setUser,
-      }
-    }>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Login/>}/>
-          <Route path='/home' element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </ToggleContext.Provider>
-  )
+    <div className="App">
+      <div className="container">
+   
+          <SideNav />
+          <Main />
+         
+      </div>
+    </div>
+  );
 }
+
+export default App;
